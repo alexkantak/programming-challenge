7cee682db8fd2bf69f625d705b27656459d1711d
Alexander Kantak <alex.kantak@live.de>
2022-04-27 18:40:09 +0200

    Create a csv reader

---
+++ src/main/java/de/exxcellent/challenge/CSVReader.java
@@ -0,0 +1,31 @@
+package de.exxcellent.challenge;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CSVReader {
+
+	private final String resourcePath;
+
+	CSVReader(String resourcePath) {
+		this.resourcePath = resourcePath;
+	}
+
+	public List<List<String>> readFile(String fileName) {
+		List<List<String>> content = new ArrayList<>();
+		try (BufferedReader br = new BufferedReader(new FileReader(resourcePath + fileName))) {
+			String line;
+			while ((line = br.readLine()) != null) {
+				String[] values = line.split(",");
+				content.add(Arrays.asList(values));
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return content;
+	}
+}
---
+++ src/test/java/de/exxcellent/challenge/CSVReaderTest.java
@@ -0,0 +1,38 @@
+package de.exxcellent.challenge;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Example JUnit 5 test case.
+ *
+ * @author Benjamin Schmid <benjamin.schmid@exxcellent.de>
+ */
+class CSVReaderTest {
+
+	private final CSVReader reader = new CSVReader("src/main/resources/de/exxcellent/challenge/");
+	private final List<List<String>> weatherData = reader.readFile("weather.csv");
+
+	@Test
+	void testHeaderData() {
+		List<String> headerTestData = Arrays.asList("Day", "MxT", "MnT", "AvT", "AvDP", "1HrP TPcpn", "PDir", "AvSp", "Dir", "MxS", "SkyC", "MxR", "Mn", "R AvSLP");
+		assertEquals(headerTestData, weatherData.get(0));
+	}
+
+	@Test
+	void testFirstRowData() {
+		List<String> rowTestData = Arrays.asList("1", "88", "59", "74", "53.8", "0", "280", "9.6", "270", "17", "1.6", "93", "23", "1004.5");
+		assertEquals(rowTestData, weatherData.get(1));
+	}
+
+	@Test
+	void testDataLength() {
+		assertEquals(31, weatherData.size());
+	}
+
+}
